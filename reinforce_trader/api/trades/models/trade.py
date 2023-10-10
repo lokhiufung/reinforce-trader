d6e49d5810d814238fdadcd4cc3ceec8f3af03ae
lokhiufung <lokhiufung123@gmail.com>
2023-10-10 11:05:47 -0400

    used s3 filekey as image

--- reinforce_trader/api/trades/models/trade.py
+++ reinforce_trader/api/trades/models/trade.py
@@ -1,18 +1,28 @@
-from typing import List, Optional
-from pydantic import BaseModel, Field
+from typing import Optional
+
+from pydantic import BaseModel
 
 # Define a Pydantic model for the Trade
 class Trade(BaseModel):
-    id: Optional[str] = Field(None, alias='_id')  # The alias='_id' allows you to use 'id' in Python but '_id' when interacting with MongoDB
-    userId: str
+    # id: Optional[str] = Field(None, alias='_id')  # The alias='_id' allows you to use 'id' in Python but '_id' when interacting with MongoDB
     strategy: str
     ticker: str
     price: float
     date: str
     size: float
     side: int
-    image: Optional[bytes] = None
-    notes: Optional[str] = None
+    image: str
+    notes: str
 
-    class Config:
-        allow_population_by_field_name = True  # This allows using the '_id' field to populate 'id'
+    # class Config:
+    #     allow_population_by_field_name = True  # This allows using the '_id' field to populate 'id'
+
+class TradeUpdate(BaseModel):
+    strategy: Optional[str] = None
+    ticker: Optional[str] = None
+    price: Optional[float] = None
+    date: Optional[str] = None
+    size: Optional[float] = None
+    side: Optional[int] = None
+    image: Optional[str] = None
+    notes: Optional[str] = None
